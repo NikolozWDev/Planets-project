0317702d70fb648662d6bd16176e3ae799cf5ce2
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-03-29 00:50:11 +0400

    added pages, datas, generated HTML

--- data/header.js
+++ data/header.js
@@ -1,20 +1,32 @@
 export function header() {
+    
     const htmlHeader = `
         <div class="main-header">
             <p class="header-title">THE PLANETS</p>
             <div class="planet-links-container">
-                <a class="planet-links">MERCURY</a>
-                <a class="planet-links">VENUS</a>
-                <a class="planet-links">EARTH</a>
-                <a class="planet-links">MARS</a>
-                <a class="planet-links">JUPITER</a>
-                <a class="planet-links">SATURN</a>
-                <a class="planet-links">URANUS</a>
-                <a class="planet-links">NEPTUNE</a>
+                <a href="mercury.html" class="planet-links">MERCURY</a>
+                <a href="venus.html" class="planet-links">VENUS</a>
+                <a href="earth.html" class="planet-links">EARTH</a>
+                <a href="mars.html" class="planet-links">MARS</a>
+                <a href="jupiter.html" class="planet-links">JUPITER</a>
+                <a href="saturn.html" class="planet-links">SATURN</a>
+                <a href="uranus.html" class="planet-links">URANUS</a>
+                <a href="neptune.html" class="planet-links">NEPTUNE</a>
             </div>
             <img class="burger-bar" src="../assets/Group.png">
         </div>
     `;
 
     document.querySelector('.header').innerHTML = htmlHeader;
+
+    let elementsClass = document.querySelectorAll('.planet-links');
+    elementsClass.forEach((element) => {
+        element.addEventListener('mouseover', () => {
+            element.style.borderTop = '3px solid rgba(65, 158, 187, 1)';
+        })
+        element.addEventListener('mouseout', () => {
+            element.style.borderTop = '';
+        })
+    })
+
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,116 @@
 import { header } from "../data/header.js";
+import { dataPlanet } from "../data/planets.js";
 header();
+
+
+function GenerateContent(index) {
+    return `
+    
+    <!-- content buttons -->
+    <div class="buttons-div delete1">
+        <button class="buttons-div-button">OVERVIEW</button>
+        <button class="buttons-div-button">STRUCTURE</button>
+        <button class="buttons-div-button">SURFACE</button>
+    </div>
+
+
+    <div class="main-content-container">
+    <!-- main selector (planet, content, content buttons) -->
+    <div class="main-selector">
+
+
+        <div class="main-img-selector">
+            <div class="image-absolute">
+            <img class="planet both1" src="${dataPlanet[index].planetImage1}">
+            <img id="planet-images" class="invisible" src="${dataPlanet[index].planetImage1}">
+            <img class="planet-2" src="${dataPlanet[index].planetImage2}">
+            <img class="planet-3" src="${dataPlanet[index].planetImage3}">
+        </div>
+        </div>
+
+
+        <div class="main-content-selector">
+
+            <div class="content-selector">
+                <p class="content-title">${dataPlanet[index].titleName}</p>
+                <p class="content-text">${dataPlanet[index].content1}
+                    </p>
+                <p class="content-source">Source : <a href="${dataPlanet[index].source}" target="_blank" class="source-link">Wikipedia</a></p>
+            </div>
+            
+            <div class="delete-it">
+            <div class="buttons-div">
+                <button class="buttons-div-button"><span class="button-number">01</span> OVERVIEW</button>
+                <button class="buttons-div-button"><span class="button-number">02</span> INTERNAL STRUCTURE</button>
+                <button class="buttons-div-button"><span class="button-number">03</span> SURFACE GEOLOGY</button>
+            </div>
+        </div>
+
+        </div>
+
+
+    </div>
+</div>
+
+
+<div class="main-buttons-container">
+    <!-- record containers -->
+    <div class="record-container">
+
+        <div class="record-selector">
+            <p class="record-left">ROTATION TIME</p>
+            <p class="record-right">${dataPlanet[index].button1Content}</p>
+        </div>
+        <div class="record-selector">
+            <p class="record-left">REVOLUTION TIME</p>
+            <p class="record-right">${dataPlanet[index].button2Content}</p>
+        </div>
+        <div class="record-selector">
+            <p class="record-left">RADIUS</p>
+            <p class="record-right">${dataPlanet[index].button3Content}</p>
+        </div>
+        <div class="record-selector">
+            <p class="record-left">AVERAGE TEMP.</p>
+            <p class="record-right">${dataPlanet[index].button4Content}</p>
+        </div>
+
+    </div>
+</div>
+    `
+
+};
+
+
+    const mercuryContent = document.querySelector('.mercury');
+    const venusContent = document.querySelector('.venus');
+    const earthContent = document.querySelector('.earth');
+    const marsContent = document.querySelector('.mars');
+    const jupiterContent = document.querySelector('.jupiter');
+    const saturnContent = document.querySelector('.saturn');
+    const uranusContent = document.querySelector('.uranus');
+    const neptuneContent = document.querySelector('.neptune');
+    
+    if (mercuryContent) {
+        mercuryContent.innerHTML = GenerateContent(0);
+    }
+    if (venusContent) {
+        venusContent.innerHTML = GenerateContent(1);
+    }
+    if(earthContent) {
+        earthContent.innerHTML = GenerateContent(2)
+    }
+    if(marsContent) {
+        marsContent.innerHTML = GenerateContent(3)
+    }
+    if(jupiterContent) {
+        jupiterContent.innerHTML = GenerateContent(4)
+    }
+    if(saturnContent) {
+        saturnContent.innerHTML = GenerateContent(5)
+    }
+    if(uranusContent) {
+        uranusContent.innerHTML = GenerateContent(6)
+    }
+    if(neptuneContent) {
+        neptuneContent.innerHTML = GenerateContent(7)
+    }
